99214a4bf7d353bf1d9fbeb75e15a4b88b626795
Muanester <stevenmutisya999@gmail.com>
2023-07-20 13:31:05 +0300

    Complete Responsive

---
+++ orders.js
@@ -0,0 +1,38 @@
+const orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '63031',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '70901',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Drone With Camera Drone',
+    productNumber: '11123',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Essential Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'GPS 4K Drone',
+    productNumber: '31856',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  }
+]
